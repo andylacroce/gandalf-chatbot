5a75047d3c7294751c3331724d503761f8f1e353
Andy Lacroce <andy.lacroce@gmail.com>
2023-11-16 17:39:33 -0500

    hide the API key

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -11,7 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   const openai = new OpenAI({
-    apiKey: "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",
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   });
 
   try {
