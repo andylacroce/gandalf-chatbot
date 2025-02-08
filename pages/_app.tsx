0540ad60be0304d72afe8c945545ab05f1f153c1
Andy Lacroce <andy.lacroce@gmail.com>
2025-02-08 14:24:43 -0500

    the ol college try

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import '@vercel/speed-insights';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Initialize Vercel Speed Insights
+    if (typeof window !== 'undefined') {
+      import('@vercel/speed-insights').then((module) => {
+        module.injectSpeedInsights();
+      });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
