1e23c0828f833a988f04950ad5ffa7d2b21d4b32
Andy Lacroce <andy.lacroce@gmail.com>
2025-03-06 14:48:52 -0500

    fix 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import '@vercel/speed-insights';
 import { Analytics } from '@vercel/analytics/react';
-import ReactDOM from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../globals.css';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  useEffect(() => {
-    // Initialize Vercel Speed Insights
-    if (typeof window !== 'undefined') {
-      import('@vercel/speed-insights').then((module) => {
-        module.injectSpeedInsights();
-      });
-
-      // Dynamically append SpeedInsights to the body
-      const speedInsightsElement = document.createElement('div');
-      document.body.appendChild(speedInsightsElement);
-      import('@vercel/speed-insights/next').then(({ SpeedInsights }) => {
-        const root = ReactDOM.createRoot(speedInsightsElement);
-        root.render(<SpeedInsights />);
-      });
-    }
-  }, []);
-
   return (
     <html lang="en">
       <head>
@@ -30,7 +13,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <body>
         {children}
         <Analytics />
-        <div id="speed-insights-container"></div>
+        <SpeedInsights />
       </body>
     </html>
   );
