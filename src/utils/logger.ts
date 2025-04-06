b807fadd893a32c32d6a08a2cbfe5de85b59e92c
Andy Lacroce <andy.lacroce@gmail.com>
2025-04-06 17:54:52 -0400

    fix: update logger format to use custom printf for improved log readability

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,7 +4,10 @@ const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.json()
+        winston.format.printf(({ timestamp, level, message, ...meta }) => {
+            const metaString = Object.keys(meta).length ? JSON.stringify(meta) : '';
+            return `[${timestamp}] [${level.toUpperCase()}]: ${message} ${metaString}`;
+        })
     ),
     transports: [new winston.transports.Console()],
 });
